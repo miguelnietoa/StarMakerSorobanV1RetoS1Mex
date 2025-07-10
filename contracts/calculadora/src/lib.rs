ab5850d7b554c6b90d095965e306c816376ad3e3
Miguel Nieto <39246879+miguelnietoa@users.noreply.github.com>
2025-07-10 16:39:11 -0600

    Update lib.rs

--- contracts/calculadora/src/lib.rs
+++ contracts/calculadora/src/lib.rs
@@ -1,23 +1,25 @@
 #![no_std]
 
-
-use soroban_sdk::{contract, contractimpl,  Env, symbol_short, Symbol};
+use soroban_sdk::{contract, contractimpl, symbol_short, Env, Symbol};
 const RESULTADO: Symbol = symbol_short!("RESULTADO");
 #[contract]
 pub struct Contract;
 
-
 #[contractimpl]
 impl Contract {
-    
-    pub fn sumar(env: Env, a:i128, b:i128) -> i128 {
-      //Implementar función que sume dos números
-      return 20;
+    pub fn sumar(env: Env, a: i128, b: i128) -> i128 {
+        //Implementar función que sume dos números
+
+        let resultado: i128 = a + b;
+
+        env.storage().instance().set(&RESULTADO, &resultado);
+
+        return resultado;
     }
 
     pub fn resultado_anterior(env: Env) -> i128 {
-           //Implementar función que retorne el valor anterior
-            return 20;
+        //Implementar función que retorne el valor anterior
+        return env.storage().instance().get(&RESULTADO).unwrap_or(0);
     }
 }
 
